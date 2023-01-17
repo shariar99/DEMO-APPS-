a6391ebb5880f73fec478239e82129cffd1bf662
Shariar Hossain <shariarhossin99@gmail.com>
2023-01-17 22:24:18 +0600

    login page

---
+++ app/src/main/java/com/shariar99/demoapps/View/Ui/Login.java
@@ -0,0 +1,16 @@
+package com.shariar99.demoapps.View.Ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shariar99.demoapps.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/shariar99/demoapps/View/Ui/Sign_up.java
@@ -0,0 +1,16 @@
+package com.shariar99.demoapps.View.Ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shariar99.demoapps.R;
+
+public class Sign_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
