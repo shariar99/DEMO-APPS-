87b6f994145fe66aa9b19f5b5f0144d2272b3b45
Shariar Hossain <shariarhossin99@gmail.com>
2023-01-17 23:09:03 +0600

    Signup add

--- app/src/main/java/com/shariar99/demoapps/View/Ui/Sign_up.java
+++ app/src/main/java/com/shariar99/demoapps/View/Ui/Sign_up.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.TestLooperManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.shariar99.demoapps.R;
 
@@ -16,6 +18,14 @@ public class Sign_up extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         Button button = findViewById(R.id.signup);
+        TextView textView = findViewById(R.id.backlogin);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Sign_up.this, Login.class);
+                startActivity(intent);
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
