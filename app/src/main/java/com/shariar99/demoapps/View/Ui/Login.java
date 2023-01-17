9bbdc16bc9f9c0960e5f216cce69272b4f1dd668
Shariar Hossain <shariarhossin99@gmail.com>
2023-01-17 22:59:27 +0600

    Signup add

--- app/src/main/java/com/shariar99/demoapps/View/Ui/Login.java
+++ app/src/main/java/com/shariar99/demoapps/View/Ui/Login.java
@@ -2,7 +2,10 @@ package com.shariar99.demoapps.View.Ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.shariar99.demoapps.R;
 
@@ -12,5 +15,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btn = findViewById(R.id.signup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/shariar99/demoapps/View/Ui/Sign_up.java
+++ app/src/main/java/com/shariar99/demoapps/View/Ui/Sign_up.java
@@ -2,7 +2,10 @@ package com.shariar99.demoapps.View.Ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.shariar99.demoapps.R;
 
@@ -12,5 +15,15 @@ public class Sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        Button button = findViewById(R.id.signup);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Sign_up.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
